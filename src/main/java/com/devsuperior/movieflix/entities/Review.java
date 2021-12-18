3fcfc466c7180058670957903a1868739268c421
Everton Souza <evrasouza@gmail.com>
2021-12-18 19:57:52 -0300

    Review Entity

--- src/main/java/com/devsuperior/movieflix/entities/Review.java
+++ src/main/java/com/devsuperior/movieflix/entities/Review.java
@@ -6,6 +6,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -19,12 +21,23 @@ public class Review implements Serializable{
 	private Long id;
 	private String text;
 	
+	@ManyToOne
+	@JoinColumn(name = "user_id")
+	private User user;
+	
+	@ManyToOne
+	@JoinColumn(name = "movie_id")
+	private Movie movie;
+	
 	public Review() {
 	}
-
-	public Review(Long id, String text) {
+	
+	public Review(Long id, String text, User user, Movie movie) {
+		super();
 		this.id = id;
 		this.text = text;
+		this.user = user;
+		this.movie = movie;
 	}
 
 	public Long getId() {
@@ -35,12 +48,28 @@ public class Review implements Serializable{
 		this.id = id;
 	}
 
-	public String getAuthority() {
+	public String getText() {
 		return text;
 	}
 
-	public void setAuthority(String text) {
+	public void setText(String text) {
 		this.text = text;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
 	}
 
 	@Override
