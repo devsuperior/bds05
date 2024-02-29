d174fa3378f9279d3cc2b5db7e3cac66da3e20c4
Paulojoserc <pjrc83@gmail.com>
2024-02-29 17:00:25 -0300

    criação dos repositories

---
+++ src/main/java/com/devsuperior/movieflix/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.movieflix.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.movieflix.entities.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long>{
+
+}
