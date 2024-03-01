16aa5754f32bb74febaa56b053695f7c312013d2
Paulojoserc <pjrc83@gmail.com>
2024-02-29 22:21:08 -0300

    Criação das classes services e dto

--- src/main/java/com/devsuperior/movieflix/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/movieflix/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.devsuperior.movieflix.entities.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
-
+	User findByEmail(String email);
 }
---
+++ src/main/java/com/devsuperior/movieflix/services/AuthService.java
@@ -0,0 +1,37 @@
+package com.devsuperior.movieflix.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.UserRepository;
+import com.devsuperior.movieflix.services.exceptions.ForbiddenException;
+import com.devsuperior.movieflix.services.exceptions.UnauthorizedException;
+
+@Service
+public class AuthService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	@Transactional(readOnly = true)
+	public User authenticated() {
+		try {
+			String username = SecurityContextHolder.getContext().getAuthentication().getName();
+			return repository.findByEmail(username);
+		}catch(Exception e) {
+			throw new UnauthorizedException("Invalid user");
+		}
+	}
+	
+	public void	validateSelf(Long userId) {
+		User user = authenticated();
+		if(!user.getId().equals(userId)) {
+			throw new ForbiddenException("Access denied");
+		}else {
+			
+		}
+	}
+}
